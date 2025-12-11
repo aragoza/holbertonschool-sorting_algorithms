1b7c7c8c2bcce922ffc77c33e069df0aacea17cb
aragoza <charlet.elliot@gmail.com>
2025-12-11 16:24:44 +0100

    write a sorting algorithm that use a selection sorted algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorted an array with a selecton algorithm
+ *
+ * @array: the array to sorted
+ * @size: the size of the array
+ *
+ * no return because void output
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, imin, temp;
+
+	for(i = 0; i < (size - 1); i++)
+	{
+		imin = i;
+
+		for(j = (i + 1); j < size; j++)
+		{
+			if(array[j] < array[imin])
+				imin = j;
+		}
+
+		temp = array[i];
+		array[i] = array[imin];
+		array[imin] = temp;
+	}
+}
