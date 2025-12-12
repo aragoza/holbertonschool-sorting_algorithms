205e513b174b6ab39de739eee4a85c8ab5a3893a
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-12-12 17:15:47 +0100

    feat: Added bubble_sort prototype

--- sort.h
+++ sort.h
@@ -23,6 +23,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+/* Ilan (0) */
+void bubble_sort(int *array, size_t size);
+
 <<<<<<< HEAD
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
