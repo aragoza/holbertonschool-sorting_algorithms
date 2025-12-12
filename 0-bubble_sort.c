d19cdf6d4a7d213a6ce6aa44da165122ee714f16
ilan-deversenne <ilan.deversenne5@gmail.com>
2025-12-12 18:26:32 +0100

    feat: Added function bubble_sort
    Bubble sort an array

---
+++ 0-bubble_sort.c
@@ -0,0 +1,24 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Bubble sort an array
+ * @array: Array
+ * @size: Size of the array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	unsigned long int i, j;
+	unsigned long int temp;
+
+	for (i = 0; i < size; i++)
+		for (j = 0; j < (size - i - 1); j++)
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+
+				print_array(array, size);
+			}
+}
